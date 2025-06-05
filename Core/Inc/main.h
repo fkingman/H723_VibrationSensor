c80132b0def004db62794f2a73a256dd04e033e7
Lzh <984906729@qq.com>
2025-06-05 16:09:09 +0800

    v1.0

---
+++ Core/Inc/main.h
@@ -0,0 +1,83 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define EN_ADC_Pin GPIO_PIN_9
+#define EN_ADC_GPIO_Port GPIOB
+#define Temp_DS_Pin GPIO_PIN_8
+#define Temp_DS_GPIO_Port GPIOB
+#define X_ADC_Pin GPIO_PIN_0
+#define X_ADC_GPIO_Port GPIOC
+#define Z_ADC_Pin GPIO_PIN_0
+#define Z_ADC_GPIO_Port GPIOA
+#define Y_ADC_Pin GPIO_PIN_4
+#define Y_ADC_GPIO_Port GPIOA
+#define LED_G_Pin GPIO_PIN_11
+#define LED_G_GPIO_Port GPIOE
+#define LED_R_Pin GPIO_PIN_12
+#define LED_R_GPIO_Port GPIOE
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
